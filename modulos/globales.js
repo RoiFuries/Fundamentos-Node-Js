52b33145b0899a76a296f3b098def795de9b881b
RoiFuries <roi.furies@gmail.com>
2021-09-05 20:48:07 -0500

    global class

---
+++ modulos/globales.js
@@ -0,0 +1,15 @@
+// let i = 0;
+
+// let intervalo = setInterval(function () {
+//     console.log('holi');
+//     if (i === 3){
+//         clearInterval(intervalo)
+//     }
+//     i ++ 
+// }, 1000)
+
+// setImmediate(function() {
+//     console.log('sup');
+// })
+
+console.log(__filename);
